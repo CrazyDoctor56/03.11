120a99da4bda44aec9d0d2b09584999ed7f62d24
Kilex <kilex111@gmail.com>
2024-11-03 13:00:56 +0200

    ggggg

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hello, World!")
+from PyQt6.QtWidgets import (QApplication, QWidget,
+                              QLabel, QVBoxLayout)
+from PyQt6.QtCore import Qt
+
+app = QApplication([])
+win = QWidget()
+
+win.setWindowTitle("Fun programm")
+win.resize(600, 500)
+
+text = QLabel("Hello, World!")
+main_line = QVBoxLayout()
+
+main_line.addWidget(text, alignment = Qt.AlignmentFlag.AlignCenter)
+
+win.setLayout(main_line)
+
+win.show()
+app.exec()
